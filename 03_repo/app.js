bac1c4dcb48475a2334a1ab32af04dae1e1c878e
Gun Kimsong <gunkimsong8@gmail.com>
2025-09-09 17:10:29 +0700

    fix wording

--- 03_repo/app.js
+++ 03_repo/app.js
@@ -50,7 +50,7 @@ app.get('/api/files/:filename', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log('Server is running on port 3000');
 });
 
 module.exports = app;
